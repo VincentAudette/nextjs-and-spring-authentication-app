bdfe378484d1cfc30fffaf2acfdca32b32aee298
VincentAudette <vincentaudette@audla.ca>
2023-07-27 15:16:22 -0400

    Started fetching users

--- frontend/components/gestion-de-mdp-view.tsx
+++ frontend/components/gestion-de-mdp-view.tsx
@@ -1,9 +1,47 @@
+import { useQuery } from "react-query";
+import LoadingQuery from "./loading-query";
+import ErrorQuery, { Error } from "./error-query";
+import { useAuth } from "context/auth-context";
+
 export default function GestionDeMotDePasseView(){
+
+    const {profile} = useAuth();
+   
+   
+    const fetchUsers = async () => {
+        const res = await fetch(`/api/getUsers?token=${profile.token}`);
+        if (!res.ok) {
+            const errorObj = await res.json();
+            errorObj.status = res.status;
+            throw errorObj;
+        }
+        return await res.json();
+    };
+    
+
+    const { data: users, isLoading, isError, error } = useQuery('users',fetchUsers);
+
+    console.log(users);
+    
+
+    
+    if (isLoading) return <LoadingQuery />
+    if (isError) return <ErrorQuery error={error as Error} />
+
    return( <div>
             <title>Gestion de mot de passe | GTI619 | Labo 5</title>
-            <div className="flex items-center justify-center mb-3">
-                <h1 className="titre-section">Gestion de mot de passe</h1>
+            <div className="flex flex-col justify-center mb-3">
+                <h1 className="titre-section mb-5">Gestion de mot de passe</h1>
                 <div className="grow"/>
+                {
+                    users.length >= 1 &&
+                    users.map(user=>(
+                        <div key={user.id} className="py-5 px-6 rounded-xl bg-neutral-50 text-neutral-950 font-bold">
+                            <p > Nom d&apos;utilisateur : {user.username}</p>
+                        </div>
+                    ))
+                }
+             
                 </div>
             </div>)
 }
---
+++ frontend/pages/api/getUsers.ts
@@ -0,0 +1,20 @@
+// pages/api/clients.js
+import axios from 'axios';
+
+export default async function handler(req, res) {
+    try {
+        const axiosRes = await axios.get(`http://localhost:8080/api/users`, {
+            headers: {
+                Authorization: `Bearer ${req.query.token}}`,
+            },
+        });
+        res.status(200).json(axiosRes.data);
+    } catch (error) {
+        if(error.response.data == "SESSION_EXPIRED"){
+            return res.status(401).json({
+                message: "Votre session a expiré.",
+            });
+        }
+        res.status(error.response?.status || 500).json({ message: error.message });
+    }
+}
